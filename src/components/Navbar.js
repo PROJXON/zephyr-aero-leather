ac0fbf43a3bfe4c97c0dcdb7a30077fa5113cdd3
davenpos <davenpos@uwindsor.ca>
2025-03-05 15:50:34 -0500

    Had mobile menu button use NavButton component too, and also removed strokeLinecap and stokeLinejoin props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,9 +72,8 @@ const Navbar = ({ initialUser }) => {
                   setCartOpen(!cartOpen);
                   setAccountOpen(false);
                 }}
+                additionalClasses="rounded-lg text-sm font-medium"
                 srOnly="Cart"
-                strokeLinecap="round"
-                strokeLinejoin="round"
                 d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"
                 text={isAuthenticated || serverUser ? "My Cart" : "Guest Cart"}
               />
@@ -98,6 +97,7 @@ const Navbar = ({ initialUser }) => {
                     setAccountOpen(!accountOpen);
                     setCartOpen(false);
                   }}
+                  additionalClasses="rounded-lg text-sm font-medium"
                   d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                   text={serverUser?.first_name || user?.first_name}
                 />
@@ -115,16 +115,12 @@ const Navbar = ({ initialUser }) => {
             )}
 
             {/* Mobile Menu Button */}
-            <button
-              type="button"
+            <NavButton
               onClick={() => setMenuOpen(!menuOpen)}
-              className="inline-flex lg:hidden items-center p-2 hover:bg-gray-100 rounded-md text-gray-900"
-            >
-              <span className="sr-only">Open Menu</span>
-              <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14" />
-              </svg>
-            </button>
+              additionalClasses="lg:hidden rounded-md"
+              srOnly="OpenMenu"
+              d="M5 7h14M5 12h14M5 17h14"
+            />
           </div>
         </div>
 
