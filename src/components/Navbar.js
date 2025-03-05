8789c5970c241d68b406951b960ee0ea07fe7585
davenpos <davenpos@uwindsor.ca>
2025-03-05 12:36:58 -0500

    Made changes to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import ZephyrLogo from "../../public/zephyrlogo.jpg";
 import { useAuth } from "@/app/context/AuthContext";
-
+import NavButton from "./NavButton";
+import NavLoggedOutBtn from "./NavLoggedOutBtn";
 
 const Navbar = ({ initialUser }) => {
   const { isAuthenticated, user, login, logout, fetchUserFromServer } = useAuth();
@@ -15,7 +16,7 @@ const Navbar = ({ initialUser }) => {
   const [accountOpen, setAccountOpen] = useState(false);
 
   useEffect(() => {
-    if (!initialUser) { 
+    if (!initialUser) {
       fetchUserFromServer();
     }
   }, [initialUser]);
@@ -32,7 +33,7 @@ const Navbar = ({ initialUser }) => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div className="flex items-center space-x-8">
-            <div className="shrink-0">
+            <Link href="/" className="shrink-0">
               <Image
                 className="w-64 h-32 object-contain"
                 src={ZephyrLogo}
@@ -41,15 +42,15 @@ const Navbar = ({ initialUser }) => {
                 height={64}
                 priority={true}
               />
-            </div>
+            </Link>
 
             {/* Desktop Menu */}
             <ul className="hidden lg:flex items-center gap-6 md:gap-8 py-3">
-              {["Home", "Best Sellers", "Gift Ideas", "auth-test", "login"].map((item) => (
+              {["Best Sellers", "Gift Ideas", "auth-test", "login"].map((item) => (
                 <li key={item}>
                   <Link
                     href={`/${item.toLowerCase().replace(/ /g, "-")}`}
-                    className="text-sm font-medium text-gray-900 hover:text-primary-700"
+                    className="text-sm font-medium text-gray-900 hover:text-primary-700 duration-300"
                   >
                     {item}
                   </Link>
@@ -61,25 +62,21 @@ const Navbar = ({ initialUser }) => {
           <div className="flex items-center lg:space-x-2">
             {/* Cart Dropdown */}
             <div className="relative">
-              <button
+              <NavButton
                 onClick={() => {
                   setCartOpen(!cartOpen);
                   setAccountOpen(false);
                 }}
-                className="inline-flex items-center p-2 hover:bg-gray-100 rounded-lg text-sm font-medium text-gray-900"
-              >
-                <span className="sr-only">Cart</span>
-                <svg className="w-5 h-5 lg:mr-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"
-                  />
-                </svg>
-                <span className="hidden sm:flex">{isAuthenticated || serverUser ? "My Cart" : "Guest Cart"}</span>
-              </button>
+                srOnly="Cart"
+                path={<path
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"
+                />}
+                text={isAuthenticated || serverUser ? "My Cart" : "Guest Cart"}
+              />
 
               {cartOpen && (
                 <div className="absolute right-0 mt-2 w-64 bg-white shadow-lg rounded-lg p-4">
@@ -89,36 +86,25 @@ const Navbar = ({ initialUser }) => {
             </div>
 
             {!isAuthenticated && !serverUser ? (
-              <ul className="text-sm font-medium text-gray-900 flex space-x-4">
-                <li>
-                  <Link href="/login" className="block px-3 py-2 hover:bg-gray-100 rounded-md">
-                    Sign In
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/register" className="block px-3 py-2 hover:bg-gray-100 rounded-md">
-                    Create Account
-                  </Link>
-                </li>
+              <ul className="text-sm font-medium text-gray-900 flex">
+                <NavLoggedOutBtn href="/login" text="Sign In" />
+                <NavLoggedOutBtn href="/register" text="Create Account" />
               </ul>
             ) : (
               <div className="relative">
-                <button
+                <NavButton
                   onClick={() => {
                     setAccountOpen(!accountOpen);
                     setCartOpen(false);
                   }}
-                  className="inline-flex items-center p-2 hover:bg-gray-100 rounded-lg text-sm font-medium text-gray-900"
-                >
-                  <svg className="w-5 h-5 mr-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                    />
-                  </svg>
-                  {serverUser?.first_name || user?.first_name}
-                </button>
+                  srOnly={null}
+                  path={<path
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />}
+                  text={serverUser?.first_name || user?.first_name}
+                />
                 {accountOpen && (
                   <div className="absolute right-0 mt-2 w-32 bg-white shadow-lg rounded-lg p-2">
                     <button
@@ -150,7 +136,7 @@ const Navbar = ({ initialUser }) => {
         {menuOpen && (
           <div className="bg-gray-50 border border-gray-200 rounded-lg py-3 px-4 mt-4">
             <ul className="text-gray-900 text-sm font-medium space-y-3">
-              {["Home", "Best Sellers", "Gift Ideas", "Games", "Electronics", "Home & Garden"].map((item) => (
+              {["Best Sellers", "Gift Ideas", "Games", "Electronics", "Home & Garden"].map((item) => (
                 <li key={item}>
                   <Link href={`/${item.toLowerCase().replace(/ /g, "-")}`} className="hover:text-primary-700">
                     {item}
