a7ea29d45bae72853be14cf4397c40fbaa5a9fe1
davenpos <davenpos@uwindsor.ca>
2025-03-05 19:19:02 -0500

    Created Tailwind component class for two pieces of code with the same classes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -105,7 +105,7 @@ const Navbar = ({ initialUser }) => {
                   <div className="absolute right-0 mt-2 w-32 bg-white shadow-lg rounded-lg p-2">
                     <button
                       onClick={handleLogout}
-                      className="block w-full px-3 py-2 text-sm text-red-600 hover:bg-gray-100 rounded-md"
+                      className="nav-button-no-svg w-full text-sm text-red-600"
                     >
                       Logout
                     </button>
