9cb406509a8cea8e964f90951a841f7b654ecb12
davenpos <davenpos@uwindsor.ca>
2025-04-19 16:33:33 -0400

    Moved useEffect for clearing cart to PaymentDetails component so that payment-success page would stay server-side

--- src/app/payment-success/page.jsx
+++ src/app/payment-success/page.jsx
@@ -1,28 +1,12 @@
-"use client"
-
-import { useEffect } from "react"
-import { useCart } from "@/app/context/CartContext"
 import fetchProducts from "../../../lib/woocommerce"
 import PaymentDetails from "@/components/PaymentDetails"
 
 export default async function PaymentSuccess() {
-    const products = await fetchProducts()
+  const products = await fetchProducts()
 
-    const { clearCart } = useCart()
-
-    useEffect(() => {
-      const alreadyCleared = localStorage.getItem("cartCleared")
-  
-      if (!alreadyCleared) {
-        clearCart()
-        localStorage.setItem("cartCleared", "true")
-      }
-    }, [])
-  
-
-    return (<div className="container mx-auto p-6">
-        <h1 className="text-3xl font-bold mb-4">Payment Successful! 🎉</h1>
-        <p>Thank you for your purchase.</p>
-        <PaymentDetails products={products} />
-    </div>)
+  return (<div className="container mx-auto p-6">
+    <h1 className="text-3xl font-bold mb-4">Payment Successful! 🎉</h1>
+    <p>Thank you for your purchase.</p>
+    <PaymentDetails products={products} />
+  </div>)
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ import { useCart } from "@/app/context/CartContext";
 import getChangeQuantity from "../../lib/getChangeQuantity"
 
 const Navbar = ({ initialUser, allProducts }) => {
-  const { isAuthenticated, user, login, logout, fetchUserFromServer } = useAuth();
+  const { isAuthenticated, user, logout, fetchUserFromServer } = useAuth();
   const [serverUser, setServerUser] = useState(initialUser || null)
   const [menuOpen, setMenuOpen] = useState(false);
   const [accountOpen, setAccountOpen] = useState(false);
