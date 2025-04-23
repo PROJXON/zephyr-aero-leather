968c08e6b063617f8e140bbe1a61b25f9c7a3981
davenpos <davenpos@uwindsor.ca>
2025-04-22 21:10:11 -0400

    Made changes to OrderHistory component; now I get a 404 error with the ordersResponse fetch

--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -3,30 +3,25 @@ import { useState, useEffect } from "react"
 import { useAuth } from "@/app/context/AuthContext"
 
 export default function OrderHistory({ token }) {
-    const { isAuthenticated } = useAuth()
+    const { user, isAuthenticated } = useAuth()
     const [orders, setOrders] = useState([])
 
     useEffect(() => {
         (async () => {
-            const userResponse = await fetch(
-                `${process.env.WOOCOMMERCE_API_URL}/wp-json/wp/v2/users/me`,
-                { headers: { Authorization: `Bearer ${token}` } }
-            )
+            if (isAuthenticated) {
+                //404 error
+                const ordersResponse = await fetch(
+                    `${process.env.WOOCOMMERCE_API_URL}/wp-json/wc/v3/orders?customer=${user.id}`,
+                    { headers: { Authorization: `Bearer ${token}` } }
+                )
 
-            const userID = userResponse.data.id
-
-            const ordersResponse = await fetch(
-                `${process.env.WOOCOMMERCE_API_URL}/wp-json/wc/v3/orders?customer=${userID}`,
-                { headers: { Authorization: `Bearer ${token}` } }
-            )
-
-            setOrders(ordersResponse.data)
-        })
+                setOrders(ordersResponse.data)
+            }
+        })()
     }, [isAuthenticated])
 
     return (<>
         {isAuthenticated ? (<div>
-            {/* Saying "No orders found even if you do make an order" */}
             {orders.length === 0 ?
                 <p>No orders found</p> :
                 <ul>
