86492609c529da23cadea59e1eb9cd13141baaf2
brandoxcode <brandon.a2001@outlook.es>
2025-05-22 21:01:32 -0400

    adding contact us functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,51 @@ import { useState } from "react";
 import { FiMapPin, FiPhone, FiMail } from "react-icons/fi"; // Importing icons
 
 export default function Contact() {
+  const [sent, setSent] = useState(false);
   const [isSending, setIsSending] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const confirmationEmail = async () => {  
+    const response = await fetch('/api/sendEmail/sendConfirmationEmail', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsSending(true);
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const response = await fetch('/api/sendEmail', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-    setIsSending(false);
-    alert("Message sent successfully!");
+    if (response.ok) {
+      confirmationEmail();
+      setIsSending(false);
+      alert("Message sent successfully!");
+      setSent(true);
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } else {
+      setIsSending(false);
+      alert("Message failed to send. Please try again.");
+    }
   };
 
   return (
@@ -59,22 +94,22 @@ export default function Contact() {
 
         <form onSubmit={handleSubmit}>
           <div className="input-group mb-4">
-            <input type="text" id="name" required className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white"/>
-            <label htmlFor="name" className="text-gray-400">Full Name</label>
+            <input type="text" id="name" required onChange={(e) => setFormData({ ...formData, name: e.target.value })} className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white"/>
+            <label htmlFor="name"  className="text-gray-400">Full Name</label>
           </div>
 
           <div className="input-group mb-4">
-            <input type="email" id="email" required className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white"/>
-            <label htmlFor="email" className="text-gray-400">Email</label>
+            <input type="email" id="email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} required className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white"/>
+            <label htmlFor="email"  className="text-gray-400">Email</label>
           </div>
 
           <div className="input-group mb-4">
-            <textarea id="message" required className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white"></textarea>
-            <label htmlFor="message" className="text-gray-400">Type your Message...</label>
+            <textarea id="message" required onChange={(e) => setFormData({ ...formData, message: e.target.value })} className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white"></textarea>
+            <label htmlFor="message"  className="text-gray-400">Type your Message...</label>
           </div>
 
-          <button type="submit" disabled={isSending} className={`w-full py-3 rounded-lg text-white font-bold ${isSending ? "bg-gray-600" : "bg-[#90D3B6] hover:bg-[#78C2A4] transition"}`}>
-            {isSending ? "Sending..." : "Send"}
+          <button type="submit" disabled={isSending || sent} className={`w-full py-3 rounded-lg text-white font-bold ${isSending ? "bg-gray-600" : "bg-[#90D3B6] hover:bg-[#78C2A4] transition"} ${sent ? "bg-green-600 cursor-not-allowed" : ""}  `}>
+            {isSending ? "Sending..." : sent ? "Sent" : "Send"}
           </button>
         </form>
       </div>
