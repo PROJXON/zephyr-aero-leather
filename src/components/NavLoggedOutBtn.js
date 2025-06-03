e7617fb7e8916a2034f3062051156617aec57817
Evan Perry <eperry2688@gmail.com>
2025-06-03 00:17:43 -0600

    Fix Navbar for Mobile, use Hamburger React

--- src/components/NavLoggedOutBtn.js
+++ src/components/NavLoggedOutBtn.js
@@ -1,14 +1,12 @@
 import Link from "next/link";
 
 const NavLoggedOutBtn = ({ href, text }) => (
-  <li>
     <Link
       href={href}
       className="inline-flex items-center p-2 hover:bg-gray-100 text-gray-900 duration-300 rounded-lg font-medium"
     >
       {text}
     </Link>
-  </li>
 );
 
 export default NavLoggedOutBtn;
