1e47b602e907f1ed85991721b10f662132a93046
Evan Perry <eperry2688@gmail.com>
2025-06-02 23:13:26 -0600

    View All button fixed to Link, styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import fetchProducts from "../../lib/woocommerce";
 import ProductList from "../components/ProductList";
 import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 60;
 
@@ -127,7 +128,12 @@ const Section = ({ title, products, link }) => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-2xl text-neutral-dark font-normal">{title}</h2>
-          <a href={link} className="text-sm text-blue-600 underline">View all</a>
+            <Link
+              href={link}
+              className="text-sm px-4 py-2 rounded bg-primary text-white hover:bg-accent transition-colors"
+            >
+              View All
+            </Link>        
         </div>
         <ProductList products={products} />
       </div>
