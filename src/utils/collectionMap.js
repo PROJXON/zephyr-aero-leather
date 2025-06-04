c00e5532b2d6161f6c830f7db1e5649f150fa1e4
Evan Perry <eperry2688@gmail.com>
2025-06-04 14:57:00 -0600

    Add favicon, Opengraph, small UI fix to main title/subtitle and hero comopnents for description

--- src/utils/collectionMap.js
+++ src/utils/collectionMap.js
@@ -17,7 +17,7 @@
 const collectionMap = {
   aviator: {
     name: "Aviator",
-    description: "For the high-flyer: travel-ready leather essentials.",
+    description: "For the high-flyer: travel-ready leather essentials",
     image: "/collections/aviatorcollection.jpg",
     productIds: [652, 614, 103, 104, 105], // Replace with real product IDs
     carouselImages: [
@@ -29,7 +29,7 @@ const collectionMap = {
   },
   explorer: {
     name: "Explorer",
-    description: "For the explorer: rugged leather staples built for the bold.",
+    description: "For the explorer: rugged leather staples built for the bold",
     image: "/collections/explorercollection.jpg",
     productIds: [106, 107, 108, 109, 110],
     carouselImages: [
@@ -41,7 +41,7 @@ const collectionMap = {
   },
   traveler: {
     name: "Traveler",
-    description: "For the jetsetter: sleek and practical leather designs.",
+    description: "For the jetsetter: sleek and practical leather designs",
     image: "/collections/travelercollection.jpg",
     productIds: [111, 112, 113, 114, 115],
     carouselImages: [
@@ -53,7 +53,7 @@ const collectionMap = {
   },
   commuter: {
     name: "Commuter",
-    description: "For the city-goer: everyday leather carry refined.",
+    description: "For the city-goer: everyday leather carry refined",
     image: "/collections/commutercollection.jpg",
     productIds: [116, 117, 118, 119, 120],
     carouselImages: [
@@ -65,7 +65,7 @@ const collectionMap = {
   },
   minimalist: {
     name: "Minimalist",
-    description: "For the sleek and simple: essentials, reimagined.",
+    description: "For the sleek and simple: essentials, reimagined",
     image: "/collections/minimalistcollection.jpg",
     productIds: [121, 122, 123, 124, 125],
     carouselImages: [
