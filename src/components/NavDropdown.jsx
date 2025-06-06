9014bde954ca4675a859dbcd5d30ced520c57f64
Evan Perry <eperry2688@gmail.com>
2025-06-06 03:46:37 -0600

    Update NavbarDropdown to have underline effect along with hover

--- src/components/NavDropdown.jsx
+++ src/components/NavDropdown.jsx
@@ -3,16 +3,31 @@ import Link from "next/link";
 
 const NavDropdown = ({ label, items, basePath, linkToBase = false }) => {
   return (
-    <li className="relative group">
-      {linkToBase ? (
-        <Link href={`/${basePath}`} className="text-lg font-medium text-black">
-          {label}
-        </Link>
-      ) : (
-        <button className="text-lg font-medium text-black">{label}</button>
-      )}
+    <li className="relative group overflow-visible">
+      <div className="relative inline-block">
+        {linkToBase ? (
+          <Link
+            href={`/${basePath}`}
+            className="text-lg font-medium text-black transition-all duration-300 relative"
+          >
+            {label}
+            {/* Underline */}
+            <span className="absolute left-1/2 -bottom-1 w-full h-[1.5px] bg-black transition-transform duration-300 scale-x-0 group-hover:scale-x-100 origin-center transform -translate-x-1/2" />
+          </Link>
+        ) : (
+          <button
+            className="text-lg font-medium text-black transition-all duration-300 relative"
+            type="button"
+          >
+            {label}
+            {/* Underline */}
+            <span className="absolute left-1/2 -bottom-1 w-full h-[1.5px] bg-black transition-transform duration-300 scale-x-0 group-hover:scale-x-100 origin-center transform -translate-x-1/2" />
+          </button>
+        )}
+      </div>
 
-      <div className="absolute left-0 w-48 bg-white shadow-md rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-50 pointer-events-none group-hover:pointer-events-auto">
+      {/* Dropdown */}
+      <div className="absolute left-0 mt-2 w-48 bg-white shadow-md rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-50 pointer-events-none group-hover:pointer-events-auto">
         <ul className="py-2">
           {items.map((item) => (
             <li key={item.slug}>
