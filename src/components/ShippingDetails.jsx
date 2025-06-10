e0e2c223e6123af48159056e46d5fc28c06eea95
davenpos <davenpos@uwindsor.ca>
2025-06-10 18:12:07 -0400

    Destructured details object in ShippingDetails component

--- src/components/ShippingDetails.jsx
+++ src/components/ShippingDetails.jsx
@@ -1,15 +1,17 @@
 import ShippingFormInput from "./ShippingFormInput"
 
 export default function ShippingDetails({ details }) {
+  const { name, address, city, postalCode, country } = details
+
   return (<div className="w-full max-w-md">
     <h2 className="text-xl font-semibold mb-2">Shipping Information</h2>
     <div className="grid gap-1">
-      <ShippingFormInput name="firstName" placeholder="First Name" value={details.name.first} />
-      <ShippingFormInput name="lastName" placeholder="Last Name" value={details.name.last} />
-      <ShippingFormInput name="address" placeholder="Address" value={details.address} />
-      <ShippingFormInput name="city" placeholder="City" value={details.city} />
-      <ShippingFormInput name="postalCode" placeholder="Postal Code" value={details.postalCode} />
-      <ShippingFormInput name="country" placeholder="Country" value={details.country} />
+      <ShippingFormInput name="firstName" placeholder="First Name" value={name.first} />
+      <ShippingFormInput name="lastName" placeholder="Last Name" value={name.last} />
+      <ShippingFormInput name="address" placeholder="Address" value={address} />
+      <ShippingFormInput name="city" placeholder="City" value={city} />
+      <ShippingFormInput name="postalCode" placeholder="Postal Code" value={postalCode} />
+      <ShippingFormInput name="country" placeholder="Country" value={country} />
     </div>
   </div>)
 }
