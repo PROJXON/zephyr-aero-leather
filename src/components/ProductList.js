ca96f5c1c7a4536c5ac8db7faee832d89fafa1f0
chiffonkla <116045907+chiffonkla@users.noreply.github.com>
2025-06-15 18:10:06 -0700

    Update style ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -30,18 +30,20 @@ export default function ProductList({ products }) {
           </Link>
 
           {/* Product Info */}
-          <div>
-            <Link href={`/product/${product.id}`}>
-              <h3 className="text-neutral-dark font-medium hover:underline">{product.name}</h3>
+          <div className="flex flex-col h-[100px]">
+            <Link href={`/product/${product.id}`} className="flex-grow">
+              <h3 className="text-xs text-neutral-dark font-medium hover:underline line-clamp-2">{product.name}</h3>
             </Link>
-            <p className="text-neutral-medium mb-2 text-right w-full">
-              {product.price ? `$${product.price}` : "Price not available"}
-            </p>
-            <div className="flex justify-end">
-              <AddToCartButton
-                productId={product.id}
-                className="py-2 px-4 text-sm font-medium bg-neutral-light text-neutral-dark rounded hover:bg-neutral-medium transition-colors"
-              />
+            <div className="mt-auto pt-2">
+              <p className="text-neutral-medium mb-2 text-right w-full">
+                {product.price ? `$${product.price}` : "Price not available"}
+              </p>
+              <div className="flex justify-end">
+                <AddToCartButton
+                  productId={product.id}
+                  className="py-2 px-4 text-sm font-medium bg-neutral-light text-neutral-dark rounded hover:bg-neutral-medium transition-colors"
+                />
+              </div>
             </div>
           </div>
         </div>
